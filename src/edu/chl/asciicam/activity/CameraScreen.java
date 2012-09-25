88cc39cc3a56baa78a3247d51aaa98ecb2cbbb39
Braaf <robin.braaf@gmail.com>
2012-09-25 14:53:00 +0200

    Added camera variable

--- src/edu/chl/asciicam/activity/CameraScreen.java
+++ src/edu/chl/asciicam/activity/CameraScreen.java
@@ -1,17 +1,33 @@
 package edu.chl.asciicam.activity;
 
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-
+/**
+ * This activity is for taking a picture. The background will be set to
+ * preview what the camera is seeing.
+ * 
+ * @author Braaf
+ *
+ */
 public class CameraScreen extends Activity {
-
+	
+	//Camera object
+	Camera asciiCamera;
+	
+	/**
+	 * 
+	 */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_screen);
     }
-
+    
+    /**
+     * 
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_camera_screen, menu);
