510fd6b088d4c1e646433489c3f9278e030a9e03
Braaf <robin.braaf@gmail.com>
2012-09-27 09:50:15 +0200

    Remade camera screen from scratch using only code from google guide, enough time was wasted here

--- src/edu/chl/asciicam/activity/CameraScreen.java
+++ src/edu/chl/asciicam/activity/CameraScreen.java
@@ -1,78 +1,60 @@
 package edu.chl.asciicam.activity;
 
 import edu.chl.asciicam.camera.CameraPreview;
+import android.app.Activity;
 import android.hardware.Camera;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.widget.FrameLayout;
+
+
 /**
  * This activity is for taking a picture. The background will be set to
  * preview what the camera is seeing.
- * 
+ * Most of the base code from http://developer.android.com/guide/topics/media/camera.html#custom-camera
  * @author Braaf
  *
  */
 public class CameraScreen extends Activity {
 	
-	//Camera object
-	Camera asciiCamera = null;
-	CameraPreview cameraView = null;
-	
-	/**
-	 * 
-	 */
+	private Camera mCamera;
+    private CameraPreview mPreview;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_screen);
-        
-        //Start the camera.
-        //openCamera();
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
     }
     
-    /**
-     * 
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_camera_screen, menu);
-        return true;
-    }
-    
-    //Called when activity is paused
-    @Override
-    public void onPause(){
-    	//Release the camera so other applications can use it
-    	//releaseCameraAndPreview();
-    }
-    
-    //Called when activity is resumed after a pause.
-    @Override
-    public void onResume(){
-    	//Regain access to camera so we can use it again.
-    	//openCamera();
-    }
-    
-    
-    //Use this method to gain access to the camera if it has been released.
-    private void openCamera(){
-    	//gain access to camera so we can use it.
-    	try{
-    		releaseCameraAndPreview();
-        	//Access the camera
-        	asciiCamera = Camera.open();
-        	
-        }catch(RuntimeException e){
-        	//If the camera is already in use
-        }
-    }
-    
-    //Release the camera
-    private void releaseCameraAndPreview() {
-        cameraView.setCamera(null);
-        if (asciiCamera != null) {
-        	asciiCamera.release();
-        	asciiCamera = null;
+	/** A safe way to get an instance of the Camera object. */
+	public static Camera getCameraInstance(){
+	    Camera c = null;
+	    try {
+	        c = Camera.open(); // attempt to get a Camera instance
+	    }
+	    catch (Exception e){
+	        // Camera is not available (in use or does not exist)
+	    }
+	    return c; // returns null if camera is unavailable
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();      // if you are using MediaRecorder, release it first
+		releaseCamera();              // release the camera immediately on pause event
+	}
+	
+	private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
         }
     }
 }
--- src/edu/chl/asciicam/camera/CameraPreview.java
+++ src/edu/chl/asciicam/camera/CameraPreview.java
@@ -1,72 +1,77 @@
 package edu.chl.asciicam.camera;
 
+import java.io.IOException;
+
 import android.content.Context;
 import android.hardware.Camera;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.ViewGroup;
+
 
 /**
- * A basic camera preview class, mostly the same as the camera guide from google.
+ * A basic camera preview class, base code from http://developer.android.com/guide/topics/media/camera.html#custom-camera
  * 
  * @author Braaf
  *
  */
-public class CameraPreview extends ViewGroup implements SurfaceHolder.Callback {
-	
-	SurfaceView asciiSurfView;
-    SurfaceHolder asciiSurfHolder;
-    Camera cam;
-	
-    /**
-     * 
-     * @param context Activity using an object of this class
-     * @param c The camera object used by activity.
-     */
-	public CameraPreview(Context context, Camera c){
-		super(context);
-		setCamera(c);
-		asciiSurfView = new SurfaceView(context);
-		addView(asciiSurfView);
-		
-		// Install a SurfaceHolder.Callback so we get notified when the
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private static final String TAG = "CameraPreview";
+	private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        mCamera = camera;
+
+        // Install a SurfaceHolder.Callback so we get notified when the
         // underlying surface is created and destroyed.
-		asciiSurfHolder = asciiSurfView.getHolder();
-		asciiSurfHolder.addCallback(this);
-		asciiSurfHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-		
-	}
-	
-	public void surfaceChanged(SurfaceHolder holder, int format, int width,
-			int height) {
-		// TODO Auto-generated method stub
-		
-	}
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        // deprecated setting, but required on Android versions prior to 3.0
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
 
-	public void surfaceCreated(SurfaceHolder holder) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+        }
+    }
 
-	public void surfaceDestroyed(SurfaceHolder holder) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+    }
 
-	@Override
-	protected void onLayout(boolean arg0, int arg1, int arg2, int arg3, int arg4) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	/**
-	 * 
-	 * @param c Camera object to be used for preview.
-	 */
-	public void setCamera(Camera c) {
-		cam = c;
-	}
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
 
-	
+        if (mHolder.getSurface() == null){
+          // preview surface does not exist
+          return;
+        }
 
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e){
+          // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+        // start preview with new settings
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+
+        } catch (Exception e){
+            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+        }
+    }
 }
