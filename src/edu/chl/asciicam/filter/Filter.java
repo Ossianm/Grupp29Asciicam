e4d4fceb1ebb497ff67633d06c4db82ef710962d
fredha <fredha@student.chalmers.se>
2012-10-10 13:03:55 +0200

    Added convert from byte array to Bitmap function and a reverse to said function

--- src/edu/chl/asciicam/filter/Filter.java
+++ src/edu/chl/asciicam/filter/Filter.java
@@ -1,14 +1,35 @@
 package edu.chl.asciicam.filter;
 
+import java.io.ByteArrayOutputStream;
+
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
 
 public abstract class Filter implements FilterInterface {
 
 	public abstract Bitmap convert(Bitmap bm);
 
-	public Bitmap arrayToBitmap(byte[] byteArray) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * Converts a given array to a Bitmap and returns the result.
+	 * @param byteArray The byte array to be converted into a Bitmap.
+	 * @return The resulting Bitmap.
+	 */
+	public Bitmap arrayToBitmap(byte[] byteArray) 	{
+		Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray , 0, byteArray.length);
+		return bitmap;
+	}
+
+	/**
+	 * Converts a given Bitmap to a byte array and returns the result.
+	 * @param bitmap The Bitmap to be converted into a byte array.
+	 * @return The resulting byte array.
+	 */
+	public byte[] bitmapToByteArray(Bitmap bitmap){
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		bitmap.compress(CompressFormat.PNG, 0 /*ignored for PNG*/, out);
+		byte[] byteArray = out.toByteArray();
+		return byteArray;
 	}
 
 }
