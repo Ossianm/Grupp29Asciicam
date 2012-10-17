6f3369eee432799bffd28dc8bb702fd8016c5479
Ossian Madisson <ossianm@gmail.com>
2012-10-17 12:09:52 +0200

    changed so asciifilter uses braafs bitmapconversion

--- src/edu/chl/asciicam/filter/AsciiFilter.java
+++ src/edu/chl/asciicam/filter/AsciiFilter.java
@@ -1,13 +1,14 @@
 package edu.chl.asciicam.filter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Typeface;
 import android.graphics.Paint.Style;
+import android.graphics.Typeface;
 import edu.chl.asciicam.util.Convert;
 
 
@@ -35,10 +36,10 @@ import edu.chl.asciicam.util.Convert;
  */
 
 public class AsciiFilter implements FilterInterface{
-	
+
 	//fontSize on the asciioutput. If changed it will change the size of the picture.
 	private static final int fontSize = 20;
-	
+
 	/**
 	 * Takes a file object and reads one line at a time. The method separates the line at each occurance of "," and puts the two strings in a TreeMap as V,K.
 	 * @param input The file object to be read from.
@@ -65,7 +66,7 @@ public class AsciiFilter implements FilterInterface{
 		}
 		return map;
 	}
-*/
+	 */
 
 	/**
 	 * Converts a bitmap into ASCII characters and returns the result.
@@ -75,13 +76,17 @@ public class AsciiFilter implements FilterInterface{
 	public Bitmap convert (Bitmap bm) {
 
 		//Declare the array with ASCII-signs. The index is used to decide what sign to translate the 5x10 rectangle to.
-		String[] signs = {" ", ",", "-", "*", "/", "r", "m", "+", "(", "¿", "x", "w", "K", "O", "&", "9", "#", "W", "$", "@" };
+		//				String[] signs = {" ", ",", "-", "*", "/", "r", "m", "+", "(", "¿", "x", "w", "K", "O", "&", "9", "#", "W", "$", "@" };
+		//		String[] signs = {" ", " ", " ", "*", "*", "m", "m", "+", "+", "¿", "¿", "w", "w", "&", "&", "#", "#", "@", "@", "@", "H", "H", "H", "H" };
+		String[] signs = {"@", "@", "@", "#", "#", "&", "&", "w", "w", "¿", "¿", "+", "+", "m", "m", "*", "*", ".", ".", " ", " "};
 		String picture = "";
 
 		int height = bm.getHeight();
 		int width = bm.getWidth();
 		int averageValue = 0;
-		
+
+		System.out.println(height);
+		System.out.println(width);
 		//Set starting values for integers used in the for-loops iterating over 5x10 pixels.
 		int h = 0;
 		int w = 0;
@@ -98,12 +103,12 @@ public class AsciiFilter implements FilterInterface{
 				}
 			}
 
-			averageValue = ((averageValue/50)/255)*20; //Calculates what index to use as the ASCII sign.
+			averageValue = ((20*averageValue/50)/255); //Calculates what index to use as the ASCII sign.
 			picture+=signs[averageValue];
 			averageValue = 0; //Reset the averageValue for the next 5 by 10 rectangle.
 
 			//If width is less than the next rectangle, start on a new row,
-			if(w+5 > width && h+10 < height){
+			if(w+5 >= width && h+10 < height){
 				x = 1;
 				y++;
 				picture+="\n";
@@ -120,43 +125,51 @@ public class AsciiFilter implements FilterInterface{
 			}
 		}
 
-		return Convert.StringToBitMap(picture);
+		//Make a list with strings, each string is a row in the picture
+		List<String> rows = new ArrayList<String>();
+		if(picture != null && picture.length() > 0){
+			for(String line: picture.split("\n")){
+				rows.add(line);
+			}
+		}
+
+		return createBitmap(rows);
 	}
-	
-    ///////////////////////////////////////////////////////////////////////
-    //This method creates the output bitmap from a List<String>.///////////
-    //Each string in the list will represent a row on the outputpicture.///
+
 	///////////////////////////////////////////////////////////////////////
-    private Bitmap createBitmap(List<String> pic){
-    	// Bitmap width = length
-    	Paint paint = new Paint();
-    	Bitmap bitmap = Bitmap.createBitmap(pic.get(0).length() * fontSize, pic.size() * fontSize, Bitmap.Config.RGB_565);
-    	//Creata canvas and put in bitmap as param so it will draw on it.
-    	Canvas canvas = new Canvas(bitmap);
-    	
-    	
-    	//Fill background with black
-    	paint.setColor(Color.BLACK);
-    	paint.setStyle(Style.FILL);
-    	canvas.drawPaint(paint);
-    	
-    	//Set color and stuff for drawing text
-    	paint.setColor(Color.WHITE); 
-    	paint.setTextSize(fontSize);
-    	//Monospace is needed for correct indentation
-    	paint.setTypeface(Typeface.MONOSPACE);
-    	//Scale or or will be shrimped in width, 1.8 seems to be the magical number. (1 is default)
-    	paint.setTextScaleX((float)1.7);
-    	//paint.setTextAlign(Paint.Align.CENTER);
-    	
-    	float x = 0, y = 0;
-    	//For each String we want to make a road. x is never changed since we always want
-    	//each row to start as far left as possible.
-    	for(String s : pic){
-    		char[] string = s.toCharArray();
-    		canvas.drawText(string, 0, string.length, x, y, paint);
-    		y += fontSize;
-    	}
-    	return bitmap;
-    }
+	//This method creates the output bitmap from a List<String>.///////////
+	//Each string in the list will represent a row on the outputpicture.///
+	///////////////////////////////////////////////////////////////////////
+	private Bitmap createBitmap(List<String> pic){
+		// Bitmap width = length
+		Paint paint = new Paint();
+		Bitmap bitmap = Bitmap.createBitmap(pic.get(0).length() * fontSize, pic.size() * fontSize, Bitmap.Config.RGB_565);
+		//Create canvas and put in bitmap as param so it will draw on it.
+		Canvas canvas = new Canvas(bitmap);
+
+
+		//Fill background with black
+		paint.setColor(Color.BLACK);
+		paint.setStyle(Style.FILL);
+		canvas.drawPaint(paint);
+
+		//Set color and stuff for drawing text
+		paint.setColor(Color.WHITE); 
+		paint.setTextSize(fontSize);
+		//Monospace is needed for correct indentation
+		paint.setTypeface(Typeface.MONOSPACE);
+		//Scale or or will be shrimped in width, 1.7 seems to be the magical number. (1 is default)
+		paint.setTextScaleX((float)1.8);
+		//paint.setTextAlign(Paint.Align.CENTER);
+
+		float x = 0, y = 0;
+		//For each String we want to make a road. x is never changed since we always want
+		//each row to start as far left as possible.
+		for(String s : pic){
+			char[] string = s.toCharArray();
+			canvas.drawText(string, 0, string.length, x, y, paint);
+			y += fontSize;
+		}
+		return bitmap;
+	}
 }
