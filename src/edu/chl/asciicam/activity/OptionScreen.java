068a4a61c36adf245c1e598c7281cf325ee6033f
AnimateX <martin.tornesson@gmail.com>
2012-10-17 15:05:18 +0200

    Added spinnermenu

--- src/edu/chl/asciicam/activity/OptionScreen.java
+++ src/edu/chl/asciicam/activity/OptionScreen.java
@@ -17,26 +17,30 @@ package edu.chl.asciicam.activity;
 //You should have received a copy of the GNU General Public License
 //along with Asciicam.  If not, see <http://www.gnu.org/licenses/>.
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class OptionScreen extends Activity {
 
 	Button back_btn;
+	ImageView brightness_icon;
+	//BrightnessView brightness_icon;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option_screen);
 		back_btn = (Button) findViewById(R.id.back);
+		brightness_icon = (ImageView) findViewById(R.id.brightness_icon);
 		initiateButtons();
+		initiateImageView();
     }
 
-	/**
-	 * This is called by to initiate the buttons and add functionality
-	 */
+	
+	//This is called by to initiate the buttons and add functionality
 	private void initiateButtons() {
 		back_btn.setOnClickListener(new View.OnClickListener() {
 
@@ -45,5 +49,13 @@ public class OptionScreen extends Activity {
 				finish();
 			}
 		});
+			
+	}	
+	
+	//This is called by to initiate the imageviews.
+	private void initiateImageView(){
+		brightness_icon.setBackgroundResource(R.drawable.brightness_temp);
+		
 	}
+	
 }
